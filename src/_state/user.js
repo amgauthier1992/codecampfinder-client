d6be1a238de586dcd2e9b4104b367a40a819040b
Andy <amgauthier1992@gmail.com>
2024-01-04 23:23:34 -0500

    fixes spacing issue with state files

--- src/_state/user.js
+++ src/_state/user.js
@@ -1,33 +1,33 @@
 import { atom } from 'recoil';
 
-const localStorageSync =
-  (key) =>
-  ({ setSelf, onSet }) => {
-    const savedValue = localStorage.getItem(key);
+const localStorageSync = (key) => ({ setSelf, onSet }) => {
+  const savedValue = localStorage.getItem(key);
+  
+  if (savedValue !== null) {
+    setSelf(JSON.parse(savedValue));
+  }
 
-    if (savedValue !== null) {
-      setSelf(JSON.parse(savedValue));
-    }
-
-    onSet((newValue, _, isReset) => {
-      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
-    });
-  };
+  onSet((newValue, _, isReset) => {
+    isReset
+      ? localStorage.removeItem(key)
+      : localStorage.setItem(key, JSON.stringify(newValue));
+  });
+};
 
 const userState = atom({
   key: 'userState',
   default: {},
-  effects: [localStorageSync('user')]
+  effects: [localStorageSync('user')],
 });
 
 const userCoursesState = atom({
   key: 'userCoursesState',
-  default: []
+  default: [],
 });
 
 const currentUserCourseState = atom({
   key: 'currentUserCourseState',
-  default: {}
+  default: {},
 });
 
 export { currentUserCourseState, userState, userCoursesState };
