b1ac3e5311ae62bff57fa66fd8dee721d311b791
Andy <amgauthier1992@gmail.com>
2024-01-09 16:25:27 -0500

    fixes responsiveness of Login page/form card

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -4,16 +4,19 @@ const styles = {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      padding: 0,
-      height: '100vh',
+      minHeight: '100vh',
       backgroundColor: palette.primary.main
     })
   }),
   FormCard: () => ({
+    width: '100%',
+    maxWidth: '400px',
     borderRadius: '10px',
     boxShadow:
       '0px 2px 1px 4px rgba(0,0,0,0.1),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 3px rgba(0,0,0,0.12)',
-    padding: '25px'
+    padding: '25px',
+    height: '100%',
+    overflowY: 'auto'
   })
 };
 
